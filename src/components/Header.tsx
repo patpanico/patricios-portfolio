32642da95b01f8e80516306ac83f198f9178ab12
patpanico <patpanico@hotmail.com>
2024-03-22 23:31:07 -0400

    project-init

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <div className="absolute"></div>;
+};
+
+export default Header;
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div className="flex h-screen bg-black">
+      <h1 className="m-auto text-3xl font-bold text-white">Hello World! :D</h1>
+    </div>
+  );
+};
+
+export default Hero;
